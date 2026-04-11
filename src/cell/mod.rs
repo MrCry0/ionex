b9420dedb3a21ab17be14555a277bae588a01c4d
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2026-04-11 10:16:55 +0200

    fixes
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/cell/mod.rs
+++ src/cell/mod.rs
@@ -373,7 +373,10 @@ impl MapCell {
     ///
     /// // central point
     /// let center = Point::new(0.5, 0.5);
-    /// let tec = cell.spatial_tec_interp(center);
+    ///
+    /// let tec = cell.spatial_tec_interp(center)
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 1.0);
     /// ```
     ///
@@ -394,19 +397,27 @@ impl MapCell {
     /// let cell = MapCell::from_unitary_tec(t0, gradient.0, gradient.1, gradient.2, gradient.3);
     ///
     /// // central point
-    /// let tec = cell.spatial_tec_interp(Point::new(0.5, 0.5));
+    /// let tec = cell.spatial_tec_interp(Point::new(0.5, 0.5))
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 0.25);
     ///
     /// // SW boundary
-    /// let tec = cell.spatial_tec_interp(Point::new(0.0, 0.0));
+    /// let tec = cell.spatial_tec_interp(Point::new(0.0, 0.0))
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 1.0);
     ///
     /// // SWern point
-    /// let tec = cell.spatial_tec_interp(Point::new(0.1, 0.1));
+    /// let tec = cell.spatial_tec_interp(Point::new(0.1, 0.1))
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 0.81);
     ///
     /// // SWwern point
-    /// let tec = cell.spatial_tec_interp(Point::new(0.01, 0.01));
+    /// let tec = cell.spatial_tec_interp(Point::new(0.01, 0.01))
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 0.9801);
     /// ```
     pub fn spatial_tec_interp(&self, point: Point<f64>) -> Result<TEC, Error> {
@@ -781,19 +792,25 @@ impl MapCell {
     /// let cell1 = MapCell::from_unitary_tec(t1, one_tec, one_tec, one_tec, one_tec);
     ///
     /// // verify central point value
-    /// let central_tec0 = cell0.spatial_tec_interp(center);
+    /// let central_tec0 = cell0.spatial_tec_interp(center)
+    ///     .unwrap();
+    ///
     /// assert_eq!(central_tec0.tecu(), 1.0);
     ///
     /// // verify central point value
-    /// let central_tec1 = cell1.spatial_tec_interp(center);
+    /// let central_tec1 = cell1.spatial_tec_interp(center)
+    ///     .unwrap();
+    ///
     /// assert_eq!(central_tec1.tecu(), 1.0);
     ///
     /// // spatial + temporal interpolation
     /// // <!> outside sampling interval
-    /// assert!(cell0.temporal_spatial_tec_interp(t_nok, center, &cell1).is_none());
+    /// assert!(cell0.temporal_spatial_tec_interp(t_nok, center, &cell1).is_ok());
     ///
     /// // spatial + temporal interpolation
-    /// let tec = cell0.temporal_spatial_tec_interp(t_ok, center, &cell1).unwrap();
+    /// let tec = cell0.temporal_spatial_tec_interp(t_ok, center, &cell1)
+    ///     .unwrap();
+    ///
     /// assert_eq!(tec.tecu(), 1.0);
     /// ```
     pub fn temporal_spatial_tec_interp(
